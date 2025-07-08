d2f7e5bb509aa65eb6be38b311a4ada5aeac1828
성민 류 <baekhun0330@naver.com>
2025-07-08 22:47:32 +0900

    초기 커밋

---
+++ Source/FPSTemplateServer.Target.cs
@@ -0,0 +1,15 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class FPSTemplateServerTarget : TargetRules
+{
+    public FPSTemplateServerTarget(TargetInfo Target) : base(Target)
+    {
+        Type = TargetType.Server;
+        DefaultBuildSettings = BuildSettingsVersion.Latest;
+        IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
+        ExtraModuleNames.Add("FPSTemplate");
+    }
+}
