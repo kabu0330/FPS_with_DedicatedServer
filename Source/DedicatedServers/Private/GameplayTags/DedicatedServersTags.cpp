e39f1c2835821a975dce028d616a2815a550e042
성민 류 <baekhun0330@naver.com>
2025-07-14 23:41:53 +0900

    Fleet 파싱 작업

---
+++ Source/DedicatedServers/Private/GameplayTags/DedicatedServersTags.cpp
@@ -0,0 +1,11 @@
+﻿#include "GameplayTags/DedicatedServersTags.h"
+
+namespace DedicatedServersTags
+{
+	namespace GameSessionsAPI
+	{
+		UE_DEFINE_GAMEPLAY_TAG_COMMENT(ListFleets, "DedicatedServersTags.GameSessionAPI.ListFleets", "List Fleets resources on the GameSession API")
+		
+		
+	}
+}
---
+++ Source/DedicatedServers/Private/UI/HTTP/HTTPRequestTypes.cpp
@@ -0,0 +1,25 @@
+﻿#include "UI/HTTP/HTTPRequestTypes.h"
+#include "DedicatedServers/DedicatedServers.h"
+
+void FDSMetaData::Dump() const
+{
+	UE_LOG(Log_DedicatedServer, Log, TEXT("MetaData:"));
+	UE_LOG(Log_DedicatedServer, Log, TEXT("httpStatusCode: %d"), httpStatusCode);
+	UE_LOG(Log_DedicatedServer, Log, TEXT("requestId: %s"), *requestId);
+	UE_LOG(Log_DedicatedServer, Log, TEXT("attempts: %d"), attempts);
+	UE_LOG(Log_DedicatedServer, Log, TEXT("totalRetryDelay: %f"), totalRetryDelay);
+	
+}
+
+void FDSListFleetsResponse::Dump() const
+{
+	UE_LOG(Log_DedicatedServer, Log, TEXT("ListFleetsResponse:"));
+	for (const FString& FleetId : FleetIds)
+	{
+		UE_LOG(Log_DedicatedServer, Log, TEXT("FleetId: %s"), *FleetId);
+	}
+	if (!NextToken.IsEmpty())
+	{
+		UE_LOG(Log_DedicatedServer, Log, TEXT("NextToken: %s"), *NextToken);
+	}
+}
