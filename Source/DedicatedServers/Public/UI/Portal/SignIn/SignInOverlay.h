2d957bbbadddb8e53eaefa50ed9afaf5b525d7e9
성민 류 <baekhun0330@naver.com>
2025-07-18 00:42:36 +0900

    로그인 및 회원가입 위젯 생성

--- Source/DedicatedServers/Public/UI/Portal/SignIn/SignInOverlay.h
+++ Source/DedicatedServers/Public/UI/Portal/SignIn/SignInOverlay.h
@@ -6,8 +6,14 @@
 #include "Blueprint/UserWidget.h"
 #include "SignInOverlay.generated.h"
 
+class UButton;
+class USuccessConfirmedPage;
+class UConfirmSignUpPage;
+class USignUpPage;
+class USignInPage;
 class UPortalManager;
 class UJoinGame;
+class UWidgetSwitcher;
 /**
  * 
  */
@@ -23,10 +29,37 @@ public:
 	UPROPERTY(EditDefaultsOnly)
 	TSubclassOf<UPortalManager> PortalManagerClass;
 
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UWidgetSwitcher> WidgetSwitcher;
+	
 protected:
 	virtual void NativeConstruct() override;
 	
 private:
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<USignInPage> SignInPage;
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<USignUpPage> SignUpPage;
+	
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UConfirmSignUpPage> ConfirmSignUpPage;
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<USuccessConfirmedPage> SuccessConfirmedPage;
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UButton> Button_SignIn_Test;
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UButton> Button_SignUp_Test;
+	
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UButton> Button_ConfirmSignUp_Test;
+	
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UButton> Button_SuccessConfirmed_Test;
+	
 	UPROPERTY()
 	TObjectPtr<UPortalManager> PortalManager;
 
@@ -35,4 +68,16 @@ private:
 
 	UFUNCTION()
 	void UpdateJoinGameStatusMessage(const FString& StatusMessage, bool bResetJoinGameButton);
+
+	UFUNCTION()
+	void ShowSignInPage();
+
+	UFUNCTION()
+	void ShowSignUpPage();
+	
+	UFUNCTION()
+	void ShowConfirmSignUpPage();
+	
+	UFUNCTION()
+	void ShowSuccessConfirmedPage();
 };
