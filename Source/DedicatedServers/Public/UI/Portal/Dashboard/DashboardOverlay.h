48c38661416a90c09228675588ab4a7d1a4c4cc5
성민 류 <baekhun0330@naver.com>
2025-07-22 02:39:17 +0900

    대시보드 위젯 추가

--- Source/DedicatedServers/Public/UI/Portal/Dashboard/DashboardOverlay.h
+++ Source/DedicatedServers/Public/UI/Portal/Dashboard/DashboardOverlay.h
@@ -6,6 +6,11 @@
 #include "Blueprint/UserWidget.h"
 #include "DashboardOverlay.generated.h"
 
+class UButton;
+class UWidgetSwitcher;
+class ULeaderboardPage;
+class UCareerPage;
+class UGamePage;
 /**
  * 
  */
@@ -13,4 +18,26 @@ UCLASS()
 class DEDICATEDSERVERS_API UDashboardOverlay : public UUserWidget
 {
 	GENERATED_BODY()
+public:
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UWidgetSwitcher> WidgetSwitcher;
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UGamePage> GamePage;
+	
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UCareerPage> CareerPage;
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<ULeaderboardPage> LeaderboardPage;
+	
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UButton> GameButton;
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UButton> CareerButton;
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UButton> LeaderboardButton;
+	
 };
